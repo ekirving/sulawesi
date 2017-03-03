49428bc3ccd210a0908bba87e6f917656965ab9c
Evan Irving-Pease <ekirving@hotmail.com>
2017-03-03 11:31:58 +0000

    increased text sizing and adjusted label position

--- Sula_Tess3.R
+++ Sula_Tess3.R
@@ -26,6 +26,10 @@ WesAndersonCol <- c(wes_palette("GrandBudapest")[c(1:2, 4)],
 # display the colour list
 # pie(rep(1,length(WesAndersonCol)), col=WesAndersonCol)
 
+# text sizing
+text_cex = 2
+point_cex = 1
+
 # how many graduated steps to show in the colour palette (larger = smoother gradient)
 palette.length = 10
 
@@ -34,7 +38,7 @@ interpol = FieldsKrigModel()
 # interpol = FieldsTpsModel()
 
 # Tess3 resolution for the interpolation surface
-resolution <- c(400,400)
+resolution <- c(100,100)
 # resolution <- c(1000,1000)
 
 # display the outline of the map, overlaid on the admixture components
@@ -80,7 +84,7 @@ render_map <- function(species.data, species.colours, map.display = TRUE, legend
        resolution = resolution,
        interpol = interpol,
        xlab = NA, ylab = NA, xaxt='n', yaxt='n',
-       main = NA, bty = 'n', cex = .6,
+       main = NA, bty = 'n', cex = point_cex,
        window = c(xlim, ylim),
        xlim =  xlim, ylim = ylim,
        col.palette = my.palette)
@@ -117,7 +121,7 @@ annotate_map <- function(segment.labels, segment.lines, species.colours, species
   segment.labels$label <- paste(segment.labels$name, " (n=", segment.labels$Freq, ")", sep="")
 
   # label the regions
-  with(segment.labels, text(Long, Lat, labels = segment.labels$label, cex = 1.2, pos = 4))
+  with(segment.labels, text(Long, Lat, labels = segment.labels$label, cex = text_cex, pos = 4))
 
   # add arrows for ambiguous label positions
   arrows(122.674413, -0.411239, 122.35, -0.394760, length = 0.1, code = 2, lwd = 2)
@@ -131,7 +135,7 @@ annotate_map <- function(segment.labels, segment.lines, species.colours, species
     species.clades <- sub("A4a", "A4", species.clades)
 
     # add the legend
-    legend(125.75, 1, legend = species.clades, fill = species.colours, cex = 1.2, bty = "n", title = legend.title)
+    legend(125.75, 1.7, legend = species.clades, fill = species.colours, cex = text_cex, bty = "n", title = legend.title)
   }
 }
 
@@ -153,14 +157,32 @@ clade2q <- function(species.data) {
 
 # adjust the label positions
 Sula_Lab$Long <- Sula_Lab$Long-0.3
-Sula_Lab['WC','Long'] <- Sula_Lab['WC','Long'] - 0.3
-Sula_Lab['WC','Lat'] <- Sula_Lab['WC','Lat'] + 0.75
-Sula_Lab['NE','Lat'] <- Sula_Lab['NE','Lat'] + 0.35
-Sula_Lab['NC','Lat'] <- Sula_Lab['NC','Lat'] + 0.10
-Sula_Lab['EC','Lat'] <- Sula_Lab['EC','Lat'] - 0.2
-Sula_Lab['SE','Lat'] <- Sula_Lab['SE','Lat'] + 0.2
-Sula_Lab['SE','Long'] <- Sula_Lab['SE','Long'] + 0.3
-Sula_Lab['BT','Long'] <- Sula_Lab['BT','Long'] + 0.2
+
+Sula_Lab['WC','Lat'] <- Sula_Lab['WC','Lat'] + 2.2 # up
+Sula_Lab['WC','Long'] <- Sula_Lab['WC','Long'] - 0.3 # left
+
+Sula_Lab['NE','Lat'] <- Sula_Lab['NE','Lat'] + 0.35 # up
+Sula_Lab['NE','Long'] <- Sula_Lab['NE','Long'] - 0.5 # left
+
+Sula_Lab['NC','Lat'] <- Sula_Lab['NC','Lat'] + 0.10 # up
+Sula_Lab['NC','Long'] <- Sula_Lab['NC','Long'] - 0.5 # left
+
+Sula_Lab['NW','Lat'] <- Sula_Lab['NW','Lat'] + 0.30 # up
+Sula_Lab['NW','Long'] <- Sula_Lab['NW','Long'] - 0.75 # left
+
+Sula_Lab['SW','Lat'] <- Sula_Lab['SW','Lat'] + 0.23 # up
+Sula_Lab['SW','Long'] <- Sula_Lab['SW','Long'] + 1.05 # right
+
+Sula_Lab['SU','Long'] <- Sula_Lab['SU','Long'] - 0.2 # left
+
+Sula_Lab['BU','Long'] <- Sula_Lab['BU','Long'] - 0.5 # left
+
+Sula_Lab['EC','Lat'] <- Sula_Lab['EC','Lat'] - 0.5 # down
+
+Sula_Lab['SE','Lat'] <- Sula_Lab['SE','Lat'] + 0.2 # up
+Sula_Lab['SE','Long'] <- Sula_Lab['SE','Long'] + 0.3 # right
+
+Sula_Lab['BT','Long'] <- Sula_Lab['BT','Long'] + 0.2 # right
 
 # start preparing the data
 location.columns <- c('Loc_Abbrev_LF', 'Longitude', 'Latitude')
@@ -170,20 +192,17 @@ anoa.data <- Anoa_All_Genetics[,c('A1_BT', 'A2_NW', 'A3_SE', 'A4_NE_WC', 'A5_NC_
 baby.data <- Baby_All_Genetics[,c('B1_WC_NW', 'B2_SE', 'B3_SU_BU', 'B4_NE', 'B5_TO', location.columns)]
 susc.data <- Sus_cel_All_Genetics[,c('S1_NW', 'S2_PE', 'S3', 'S4_SE_BT', 'S5_WC_SW', 'S6_BU', 'S7_EC', location.columns)]
 
-# TODO setup the STRUCTURE colour lists
-# anoa.colours <- c('#95afdf', '#d7664d', '#fe8b8d', '#a6b6bb', '#d4daf9')
-# baby.colours <- c('#f5cc9c', '#ecb8d3', '#e09568', '#fe8b8d', '#d4daf9')
-# susc.colours <- c('#95afdf', '#f5cc9c', '#a4dfea', '#fe8b8d', '#d4daf9', '#d7664d', '#f7c8ce')
+# setup the STRUCTURE colour lists
 anoa.colours <- c('#a6b6bb', '#95afdf', '#d4daf9', '#fe8b8d', '#d7664d')
 baby.colours <- c('#ecb8d3', '#d4daf9', '#f5cc9c', '#e09568', '#fe8b8d')
 susc.colours <- c('#95afdf', '#a4dfea', '#f7c8ce', '#d4daf9', '#fe8b8d', '#f5cc9c', '#d7664d')
 
-# get the cluster data
+# get the mtDNA clades and transpose them into a Q matrix
 anoa.data2 <- clade2q(Anoa_All_Genetics[,c('LF_Anoa_Clust', location.columns)])
 baby.data2 <- clade2q(Baby_All_Genetics[,c('LF_Baby_Clust', location.columns)])
 susc.data2 <- clade2q(Sus_cel_All_Genetics[,c('LF_Sus_cel_Clust', location.columns)])
 
-# TODO setup the mtDNA colour lists
+# setup the mtDNA colour lists
 anoa.colours2 <- c('#e09568', '#95afdf', '#d7664d', '#fe8b8d', '#a6b6bb', '#d4daf9')
 baby.colours2 <- c('#f5cc9c', '#ecb8d3', '#e09568', '#fe8b8d', '#d4daf9', '#95afdf')
 susc.colours2 <- c('#95afdf', '#f5cc9c', '#a4dfea', '#b5b077', '#fe8b8d')
@@ -199,8 +218,8 @@ render_map(baby.data2, baby.colours2, map.display, 'Clades')
 render_map(susc.data2, susc.colours2, map.display, 'Clades')
 
 # STRUCTURE maps
-render_map(anoa.data, anoa.colours, map.display, 'Ancestral Pops (K)')
-render_map(baby.data, baby.colours, map.display, 'Ancestral Pops (K)')
-render_map(susc.data, susc.colours, map.display, 'Ancestral Pops (K)')
+render_map(anoa.data, anoa.colours, map.display, 'Ancestral\n(K)')
+render_map(baby.data, baby.colours, map.display, 'Ancestral\n(K)')
+render_map(susc.data, susc.colours, map.display, 'Ancestral\n(K)')
 
 dev.off()
